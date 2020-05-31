870db6991ea028a3c32c48e7587f6836ae7e0038
the_E <minecraft.etto@gmail.com>
2020-05-31 18:19:22 +0200

    Build info added

---
+++ cmake-build-debug/CMakeFiles/branches.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/OneDrive - University of Pisa/Shared Projects/branches/branches_core/algebraNode/algebraNode.cpp" "D:/OneDrive - University of Pisa/Shared Projects/branches/cmake-build-debug/CMakeFiles/branches.dir/branches_core/algebraNode/algebraNode.cpp.obj"
+  "D:/OneDrive - University of Pisa/Shared Projects/branches/branches_core/algebraParser/algebraParser.cpp" "D:/OneDrive - University of Pisa/Shared Projects/branches/cmake-build-debug/CMakeFiles/branches.dir/branches_core/algebraParser/algebraParser.cpp.obj"
+  "D:/OneDrive - University of Pisa/Shared Projects/branches/branches_core/algebra_tools_/algebra_tools_.cpp" "D:/OneDrive - University of Pisa/Shared Projects/branches/cmake-build-debug/CMakeFiles/branches.dir/branches_core/algebra_tools_/algebra_tools_.cpp.obj"
+  "D:/OneDrive - University of Pisa/Shared Projects/branches/branches_core/exprNode/exprNode.cpp" "D:/OneDrive - University of Pisa/Shared Projects/branches/cmake-build-debug/CMakeFiles/branches.dir/branches_core/exprNode/exprNode.cpp.obj"
+  "D:/OneDrive - University of Pisa/Shared Projects/branches/branches_core/funcNode/funcNode.cpp" "D:/OneDrive - University of Pisa/Shared Projects/branches/cmake-build-debug/CMakeFiles/branches.dir/branches_core/funcNode/funcNode.cpp.obj"
+  "D:/OneDrive - University of Pisa/Shared Projects/branches/branches_core/varNode/varNode.cpp" "D:/OneDrive - University of Pisa/Shared Projects/branches/cmake-build-debug/CMakeFiles/branches.dir/branches_core/varNode/varNode.cpp.obj"
+  "D:/OneDrive - University of Pisa/Shared Projects/branches/main.cpp" "D:/OneDrive - University of Pisa/Shared Projects/branches/cmake-build-debug/CMakeFiles/branches.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_RC
+  "D:/OneDrive - University of Pisa/Shared Projects/branches/resources/res.rc" "D:/OneDrive - University of Pisa/Shared Projects/branches/cmake-build-debug/CMakeFiles/branches.dir/resources/res.rc.res"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/branches.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/branches.dir/branches_core/algebraNode/algebraNode.cpp.obj"
+  "CMakeFiles/branches.dir/branches_core/algebraParser/algebraParser.cpp.obj"
+  "CMakeFiles/branches.dir/branches_core/algebra_tools_/algebra_tools_.cpp.obj"
+  "CMakeFiles/branches.dir/branches_core/exprNode/exprNode.cpp.obj"
+  "CMakeFiles/branches.dir/branches_core/funcNode/funcNode.cpp.obj"
+  "CMakeFiles/branches.dir/branches_core/varNode/varNode.cpp.obj"
+  "CMakeFiles/branches.dir/main.cpp.obj"
+  "CMakeFiles/branches.dir/resources/res.rc.res"
+  "branches.exe"
+  "branches.exe.manifest"
+  "branches.lib"
+  "branches.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX RC)
+  include(CMakeFiles/branches.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
