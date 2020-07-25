5cd9b9aebc81b82e01029ac8a3ede130e0e6158d
the_E <minecraft.etto@gmail.com>
2020-07-25 07:36:53 +0200

    Added colored text, now everything is happier, even errors (exception handling was improved)

---
+++ graphics/colors.h
@@ -0,0 +1,75 @@
+//
+// Created by the_E on 20/07/25.
+//
+
+#ifndef BRANCHES_COLORS_H
+#define BRANCHES_COLORS_H
+
+#include <string>
+#include <map>
+
+std::string colorize(const std::string &text, const std::string &fg = "Default", const std::string &bg = "Default",
+                     const std::string &format = "Default", const std::string &reset = "All")
+{
+    std::map<std::string, std::string> formatMap(
+            {{"Default",    "0"},
+             {"Bold",       "1"},
+             {"Dim",        "2"},
+             {"Underlined", "3"},
+             {"Blink",      "5"},
+             {"Reverse",    "7"},
+             {"Hidden",     "8"}}
+    );
+    std::map<std::string, std::string> foregroundMap(
+            {{"Default",       "39"},
+             {"Black",         "30"},
+             {"Red",           "31"},
+             {"Green",         "32"},
+             {"Yellow",        "33"},
+             {"Blue",          "34"},
+             {"Magenta",       "35"},
+             {"Cyan",          "36"},
+             {"Light Gray",    "37"},
+             {"Dark Gray",     "90"},
+             {"Light Red",     "91"},
+             {"Light Green",   "92"},
+             {"Light Yellow",  "93"},
+             {"Light Blue",    "94"},
+             {"Light Magenta", "95"},
+             {"Light Cyan",    "96"},
+             {"White",         "97"}}
+    );
+    std::map<std::string, std::string> backgroundMap(
+            {{"Default",       "49"},
+             {"Black",         "40"},
+             {"Red",           "41"},
+             {"Green",         "42"},
+             {"Yellow",        "43"},
+             {"Blue",          "44"},
+             {"Megenta",       "45"},
+             {"Cyan",          "46"},
+             {"Light Gray",    "47"},
+             {"Dark Gray",     "100"},
+             {"Light Red",     "101"},
+             {"Light Green",   "102"},
+             {"Light Yellow",  "103"},
+             {"Light Blue",    "104"},
+             {"Light Magenta", "105"},
+             {"Light Cyan",    "106"},
+             {"White",         "107"}}
+    );
+    std::map<std::string, std::string> resetMap(
+            {{"All",        "0"},
+             {"Bold",       "21"},
+             {"Dim",        "22"},
+             {"Underlined", "24"},
+             {"Blink",      "25"},
+             {"Reverse",    "27"},
+             {"Hidden",     "28"}}
+    );
+    ///"\033[{FORMAT_ATTRIBUTE};{FORGROUND_COLOR};{BACKGROUND_COLOR}m{TEXT}\033[{RESET_FORMATE_ATTRIBUTE}m"
+    return "\033[" + formatMap[format] + ";" + foregroundMap[fg] + ";" + backgroundMap[bg] + "m" + text +
+           (reset != "None" ? ("\033[" + resetMap[reset] + "m") : "");
+}
+
+#endif //BRANCHES_COLORS_H
--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 #include "graphics/win3d.h"
 #include "graphics/winPCurve.h"
 #include "functions/numericIntegration.h"
+#include "graphics/colors.h"
 
 using namespace std;
 
-int main();
+int main(int argc, char **argv);
 
 int readCommand(const string &cmd)
 {
@@ -27,101 +28,101 @@ int readCommand(const string &cmd)
         return 1;
     } else if (cmd == "help")
     {
-        cout << "help:\n"
-                "\twill show this message\n"
-                "store|st [<name>=]<function>:\n"
-                "\twill add function to the list of drawn functions\n"
-                "\tunder the label <name>, if no \"=\"is found\n"
-                "\tthe name will be the function itself\n"
-                "remove|rm <name>|stored:\n"
-                "\tif the arg is equal to \"stored\" all the stored functions\n"
-                "\twill be removed from the list\n"
-                "\twill remove the function under the label <name>\n"
-                "list|ls:\n"
-                "\twill display all the current labels and functions in the list\n"
-                "\tin the format <name>=<function>\n"
-                "storeVal|stv [<name>=]<expression>:\n"
-                "\twill add a CONSTANT expression to the list of values\n"
-                "\tunder the label <name>\n"
-                "removeVal|rmv <name>|stored:\n"
-                "\tif the arg is equal to \"stored\" all the stored values\n"
-                "\twill be removed from the list\n"
-                "\twill remove the value under the label <name>\n"
-                "listVal|lsv:\n"
-                "\twill display all the current labels and values in the list\n"
-                "\tin the format <name>=<value>\n"
-                "evaluate|eval <expression>\n"
-                "\twill display the value of the expression (must be constant)\n"
-                "\tafter all the values has been substituted\n"
-                "\tnumericIntegration|nIntegration|nint <function> <from> <to>:\n"
-                "\t\tevaluate an approximation for the integral of function from\n"
-                "\t\tand to the specified values\n"
-                "draw <function>|<name>|stored:\n"
-                "\tif the arg is equal to \"stored\" will draw all \n"
-                "\tthe functions in the list, if the arg is a function it\n"
-                "\twill draw the function defined in the args\n"
-                "\tyou can use the name of a stored function to draw it\n"
-                "\t(function must be R->R, with x as the independent variable)\n"
-                "drawCurve <function>|<name>|(WIP:stored):\n"
-                "\tif the arg is equal to \"stored\" will draw all \n"
-                "\tthe functions in the list, if the arg is a function it\n"
-                "\twill draw the function defined in the args\n"
-                "\tyou can use the name of a stored function to draw it\n"
-                "\t(function must be R->R^2, with t as the independent variable,\n"
-                "\tthe format for a vector in R^2 is {x(t),y(t)})\n"
-                "draw3d <function>|<name>\n"
-                "\tsame as draw command but the domain is assumed to be R^2,\n"
-                "\tso a 3d graph is shown (rotating by default, see set rotating)\n"
-                "study <function>|<name>:\n"
-                "\tdraws the study of the required function or stored function\n"
-                "\thighlights maxima, minima and inflections, shows a different background\n"
-                "\t\tred:\n"
-                "\t\t\tincreasing interva\n"
-                "\t\tblue:\n"
-                "\t\t\tdecreasing interval\n"
-                "\t\tyellow:\n"
-                "\t\t\tconcave interval\n"
-                "\t\tcyan:\n"
-                "\t\t\tconvex interval\n"
-                "\tif the function does not exist on a certain interval it will be\n"
-                "\tcovered from a black red-crossed background\n"
-                "derivative|diff [store] <function>|<name>:\n"
-                "\tprints to the terminal the derivative of <function>\n"
-                "\tif the name of a stored function is provided instead, it prints\n"
-                "\tthe derivative of the stored function\n"
-                "\tif the optional argument store is provided, it will store the derivative\n"
-                "partial <variable> [store] <function>|<name>\n"
-                "\tsame as derivative command but does a partial derivative on\n"
-                "\tthe chosen variable\n"
-                "set <parameter> <value>\n"
-                "\tsets the parameter to the given value\n"
-                "\tparameters:\n"
-                "\t\tzoom (double):\n"
-                "\t\t\tsets the zoom for the draw function\n"
-                "\t\tprecision (int):\n"
-                "\t\t\tsets how many lines are used for the draw function\n"
-                "\t\tdrawGrid (bool):\n"
-                "\t\t\tchoose if to draw the grid\n"
-                "\t\tdrawAxis (bool):\n"
-                "\t\t\tchoose if to draw the x and y axis\n"
+        cout << colorize("help:", "Cyan") + "\n"
+                                            "\twill show this message\n"
+                + colorize("store|st [<name>=]<function>:", "Cyan") + "\n"
+                                                                      "\twill add function to the list of drawn functions\n"
+                                                                      "\tunder the label <name>, if no \"=\"is found\n"
+                                                                      "\tthe name will be the function itself\n"
+                + colorize("remove|rm <name>|stored:", "Cyan") + "\n"
+                                                                 "\tif the arg is equal to \"stored\" all the stored functions\n"
+                                                                 "\twill be removed from the list\n"
+                                                                 "\twill remove the function under the label <name>\n"
+                + colorize("list|ls:", "Cyan") + "\n"
+                                                 "\twill display all the current labels and functions in the list\n"
+                                                 "\tin the format <name>=<function>\n"
+                + colorize("storeVal|stv [<name>=]<expression>:", "Cyan") + "\n"
+                                                                            "\twill add a CONSTANT expression to the list of values\n"
+                                                                            "\tunder the label <name>\n"
+                + colorize("removeVal|rmv <name>|stored:", "Cyan") + "\n"
+                                                                     "\tif the arg is equal to \"stored\" all the stored values\n"
+                                                                     "\twill be removed from the list\n"
+                                                                     "\twill remove the value under the label <name>\n"
+                + colorize("listVal|lsv:", "Cyan") + "\n"
+                                                     "\twill display all the current labels and values in the list\n"
+                                                     "\tin the format <name>=<value>\n"
+                + colorize("evaluate|eval <expression>", "Cyan") + "\n"
+                                                                   "\twill display the value of the expression (must be constant)\n"
+                                                                   "\tafter all the values has been substituted\n"
+                                                                   "\tnumericIntegration|nIntegration|nint <function> <from> <to>:\n"
+                                                                   "\t\tevaluate an approximation for the integral of function from\n"
+                                                                   "\t\tand to the specified values\n"
+                + colorize("draw <function>|<name>|stored:", "Cyan") + "\n"
+                                                                       "\tif the arg is equal to \"stored\" will draw all \n"
+                                                                       "\tthe functions in the list, if the arg is a function it\n"
+                                                                       "\twill draw the function defined in the args\n"
+                                                                       "\tyou can use the name of a stored function to draw it\n"
+                                                                       "\t(function must be R->R, with x as the independent variable)\n"
+                + colorize("drawCurve <function>|<name>|(WIP:stored):", "Cyan") + "\n"
+                                                                                  "\tif the arg is equal to \"stored\" will draw all \n"
+                                                                                  "\tthe functions in the list, if the arg is a function it\n"
+                                                                                  "\twill draw the function defined in the args\n"
+                                                                                  "\tyou can use the name of a stored function to draw it\n"
+                                                                                  "\t(function must be R->R^2, with t as the independent variable,\n"
+                                                                                  "\tthe format for a vector in R^2 is {x(t),y(t)})\n"
+                + colorize("draw3d <function>|<name>:", "Cyan") + "\n"
+                                                                  "\tsame as draw command but the domain is assumed to be R^2,\n"
+                                                                  "\tso a 3d graph is shown (rotating by default, see set rotating)\n"
+                + colorize("study <function>|<name>:", "Cyan") + "\n"
+                                                                 "\tdraws the study of the required function or stored function\n"
+                                                                 "\thighlights maxima, minima and inflections, shows a different background\n"
+                                                                 "\t\tred:\n"
+                                                                 "\t\t\tincreasing interva\n"
+                                                                 "\t\tblue:\n"
+                                                                 "\t\t\tdecreasing interval\n"
+                                                                 "\t\tyellow:\n"
+                                                                 "\t\t\tconcave interval\n"
+                                                                 "\t\tcyan:\n"
+                                                                 "\t\t\tconvex interval\n"
+                                                                 "\tif the function does not exist on a certain interval it will be\n"
+                                                                 "\tcovered from a black red-crossed background\n"
+                + colorize("derivative|diff [store] <function>|<name>:", "Cyan") + "\n"
+                                                                                   "\tprints to the terminal the derivative of <function>\n"
+                                                                                   "\tif the name of a stored function is provided instead, it prints\n"
+                                                                                   "\tthe derivative of the stored function\n"
+                                                                                   "\tif the optional argument store is provided, it will store the derivative\n"
+                + colorize("partial <variable> [store] <function>|<name>:", "Cyan") + "\n"
+                                                                                      "\tsame as derivative command but does a partial derivative on\n"
+                                                                                      "\tthe chosen variable\n"
+                + colorize("set <parameter> <value>:", "Cyan") + "\n"
+                                                                 "\tsets the parameter to the given value\n"
+                                                                 "\tparameters:\n"
+                                                                 "\t\tzoom (double):\n"
+                                                                 "\t\t\tsets the zoom for the draw function\n"
+                                                                 "\t\tprecision (int):\n"
+                                                                 "\t\t\tsets how many lines are used for the draw function\n"
+                                                                 "\t\tdrawGrid (bool):\n"
+                                                                 "\t\t\tchoose if to draw the grid\n"
+                                                                 "\t\tdrawAxis (bool):\n"
+                                                                 "\t\t\tchoose if to draw the x and y axis\n"
                 "\t\trotating (bool):\n"
                 "\t\t\tchoose if the draw3d result is rotating\n"
                 "\t\tcurveFrom (double):\n"
                 "\t\t\tchose the 'a' value in the [a,b] interval in which the\n"
-                "\t\t\tcurves are defined\n"
-                "\t\tcurveTo (double):\n"
-                "\t\t\tchose the 'b' value in the [a,b] interval in which the\n"
-                "\t\t\tcurves are defined\n"
-                "\t\tdx (double):\n"
-                "\t\t\tchose the interval between slices in the nint command\n"
-                "\t\t\tthe smaller, the higher the precision\n"
+                                                                 "\t\t\tcurves are defined\n"
+                                                                 "\t\tcurveTo (double):\n"
+                                                                 "\t\t\tchose the 'b' value in the [a,b] interval in which the\n"
+                                                                 "\t\t\tcurves are defined\n"
+                                                                 "\t\tdx (double):\n"
+                                                                 "\t\t\tchose the interval between slices in the nint command\n"
+                                                                 "\t\t\tthe smaller, the higher the precision\n"
                 //"\t\tdrawText (bool):(currently unavailable)\n"
                 //"\t\t\tchoose if to draw the function in the left high corner\n"
                 //"\tuse \"default\" as value to reset the value\n"
-                "exit:\n"
-                "\twill close the program\n"
-                "\n"
-                "credits to Ettore Ricci\n";
+                + colorize("exit:", "Cyan") + "\n"
+                                              "\twill close the program\n"
+                                              "\n"
+                                              "credits to Ettore Ricci\n";
         return 0;
     } else if (cmd == "set")
     {
@@ -195,10 +196,9 @@ int readCommand(const string &cmd)
                 else if (tmp == "true")
                     drawAxis = true;
             }*/
-        } catch (algebra_tools_::except &e)
+        } catch (...)
         {
-            cout << e.what() << endl;
-            return -1;
+            throw;
         }
         return 0;
     } else if (cmd == "store" || cmd == "st")
@@ -239,10 +239,9 @@ int readCommand(const string &cmd)
             sym.erase(0, sym.find('=') + 1);
             algebraParser sp(sym);
             symbols.insert({sName, sp.evaluate(symbols)});
-        } catch (algebra_tools_::except &e)
+        } catch (...)
         {
-            cout << e.what() << endl;
-            return -1;
+            throw;
         }
         return 0;
     } else if (cmd == "removeVal" || cmd == "rmv")
@@ -374,10 +373,9 @@ int readCommand(const string &cmd)
             algebraParser toEv(args);
             auto tmp = toEv.evaluate(symbols);
             cout << args << "=" << tmp << endl;
-        } catch (algebra_tools_::except &e)
+        } catch (...)
         {
-            cout << e.what() << endl;
-            return -1;
+            throw;
         }
 
         return 0;
@@ -490,32 +488,36 @@ int readCommand(const string &cmd)
                  << (dTo - dFrom) / dx << " slices..." << endl;
             auto sum = numericIntegration(toInt, dFrom, dTo, dx, symbols);
             cout << "I(" << toInt << ")=" << sum << endl;
-        } catch (algebra_tools_::except &e)
+        } catch (...)
         {
-            cout << e.what() << endl;
-            return -1;
+            throw;
         }
         return 0;
     }
-    return -1;
+    throw algebra_tools_::except("Command not available");
 }
 
-int main()
+int main(int argc, char **argv)
 {
     string operation;
-    cout << "Welcome to BRANCHES\n"
-            "a simple graphing terminal\n"
-            "by Ettore Ricci\n\n"
-            "type \"help\" for a list of all the available commands\n\n";
+    cout << "Welcome to " + colorize("BRANCHES", "Cyan") + "\n"
+                                                           "a simple graphing terminal\n"
+                                                           "by Ettore Ricci\n\n"
+                                                           "type \"help\" for a list of all the available commands\n\n";
     int ret = 0;
     while (ret != 1)
     {
-        cout << (ret == 0 ? ">>" : "x>");
-        cin >> operation;
-        ret = readCommand(operation);
-        if (ret == -1)
+        try
+        {
+            cout << (ret == 0 ? ">>" : "x>") << colorize("", "Green", "Default", "Default", "None");
+            cin >> operation;
+            cout << colorize("");
+            ret = readCommand(operation);
+        } catch (algebra_tools_::except &e)
         {
             cin.ignore(100, '\n');
+            cout << colorize(e.what(), "Red") << endl;
+            ret = -1;
         }
     }
 
